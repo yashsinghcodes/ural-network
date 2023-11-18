ee2a655adf07b56de5684f427dd18caeaafeca0f
yashsinghcodes <yash9vardhan@gmail.com>
2023-11-18 20:48:46 +0000

    train function for the nn

--- gopher/main.go
+++ gopher/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-    "gonum.org/v1/gonum/mat"    
+	"math"
+	"gonum.org/v1/gonum/mat"
 )
-
 
 type neuralNetwork struct {
     config neuralNetConfig
@@ -24,3 +24,54 @@ type neuralNetConfig struct {
 func newNetwork(config neuralNetConfig) *neuralNetwork{
     return &neuralNetwork{config: config}
 }
+
+func sigmoid(x float64) float64 {
+    return 1.0 / (1.0 + math.Exp(-x))
+}
+
+func sigmoidPrime(x float64) float64 {
+    return sigmoid(x) * (1.0 - sigmoid(x))
+}
+
+func (nn *neuralNetwork) train(x, y *mat.Dense) error {
+    
+    randSource := rand.NewSource(time.Now().UnoxNano())
+    randGen := rand.New(randSource)
+
+    wHidden := mat.NewDense(nn.config.inputNeurons, nn.config.hiddenNeuron, nil)
+    bHidden := mat.NewDense(1, nn.config.hiddenNeuron, nil)
+    wOut := mat.NewDense(nn.config.hiddenNeuron, nn.config.outputNeurons, nil)
+    bOut := mat.NewDense(1, nn.config.outputNeurons, nil)
+
+    wHiddenRaw := wHidden.RawMatrix().Data
+    bHiddenRaw := bHidden.RawMatrix().Data
+    wOutRaw := wOut.RawMatrix().Data
+    bOutRaw := bOut.RawMatrix().Data
+
+    for _, param := range [][]float64{
+        wHiddenRaw,
+        bHiddenRaw,
+        wOutRaw,
+        bOutRaw,
+    } {
+        for i := range param {
+            param[i] = randGen.Float64()
+        }
+    }
+
+    
+    output := new(mat.Dense)
+    
+    if err := nn.backpropagate(x, y, wHidden, bHidden, wOut, bOut, output); err != nil {
+        return err
+    }
+
+    nn.wHidden = wHidden
+    nn.bHidden = bHidden
+    nn.wOut = wOut
+
+    return nil
+}
+
+
+// TODO: Write a backpropagate function for the NN.
